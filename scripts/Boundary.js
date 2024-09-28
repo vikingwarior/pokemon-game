79fe11a77f754e61042998f4951d37441db564db
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-09-29 02:22:03 +0530

    Formatting with prettier

--- scripts/Boundary.js
+++ scripts/Boundary.js
@@ -1,17 +1,16 @@
 class Boundary {
-    static height = 48;
-    static width = 48;
+  static height = 48;
+  static width = 48;
 
-    constructor({position}) {
-        this.position = position;
-    }
+  constructor({ position }) {
+    this.position = position;
+  }
 
-    draw(canvasPlane) {
-        const { x, y } = this.position;
-        canvasPlane.fillStyle = 'red';
-        canvasPlane.fillRect(x, y, Boundary.width, Boundary.height);
-    }
-
+  draw(canvasPlane) {
+    const { x, y } = this.position;
+    canvasPlane.fillStyle = "red";
+    canvasPlane.fillRect(x, y, Boundary.width, Boundary.height);
+  }
 }
 
 export default Boundary;
--- scripts/Sprite.js
+++ scripts/Sprite.js
@@ -1,29 +1,29 @@
 class Sprite {
-    constructor({ position, image, frames = { max: 1 } }) {
-        this.position = position;
-        this.image = image;
-        this.frames = frames;
+  constructor({ position, image, frames = { max: 1 } }) {
+    this.position = position;
+    this.image = image;
+    this.frames = frames;
 
-        image.onload = () => {
-            this.width = image.width / frames.max;
-            this.height = image.height;
-        }
-    }
+    image.onload = () => {
+      this.width = image.width / frames.max;
+      this.height = image.height;
+    };
+  }
 
-    draw(canvasPlane) {
-        const { x, y } = this.position;
-        const { max } = this.frames;
-        canvasPlane.drawImage(this.image, // image
-            0, // sx -> start clipping X coordinate
-            0, // sy -> start clipping Y coordinate
-            this.width, // ex -> end clipping X coordinate
-            this.height, // ey -> end clipping on the Y coordinate
-            x, // x axis offset
-            y, // y axis offset
-            this.width, // width of the image
-            this.height // height of the image
-        );
-    }
+  draw(canvasPlane) {
+    const { x, y } = this.position;
+    canvasPlane.drawImage(
+      this.image, // image
+      0, // sx -> start clipping X coordinate
+      0, // sy -> start clipping Y coordinate
+      this.width, // ex -> end clipping X coordinate
+      this.height, // ey -> end clipping on the Y coordinate
+      x, // x axis offset
+      y, // y axis offset
+      this.width, // width of the image
+      this.height // height of the image
+    );
+  }
 }
 
 export default Sprite;
