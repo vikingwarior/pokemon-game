941959f42638dc2b42cd4b2645e3d65a1abfcd5f
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-09-29 02:09:34 +0530

    Refactors Sprite class to contain player sprite

--- scripts/Sprite.js
+++ scripts/Sprite.js
@@ -1,12 +1,28 @@
 class Sprite {
-    constructor({ position, image }) {
+    constructor({ position, image, frames = { max: 1 } }) {
         this.position = position;
         this.image = image;
+        this.frames = frames;
+
+        image.onload = () => {
+            this.width = image.width / frames.max;
+            this.height = image.height;
+        }
     }
 
     draw(canvasPlane) {
         const { x, y } = this.position;
-        canvasPlane.drawImage(this.image, x, y);
+        const { max } = this.frames;
+        canvasPlane.drawImage(this.image, // image
+            0, // sx -> start clipping X coordinate
+            0, // sy -> start clipping Y coordinate
+            this.width, // ex -> end clipping X coordinate
+            this.height, // ey -> end clipping on the Y coordinate
+            x, // x axis offset
+            y, // y axis offset
+            this.width, // width of the image
+            this.height // height of the image
+        );
     }
 }
 
