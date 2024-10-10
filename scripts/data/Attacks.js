4d951eef401c9908e08b28ad8a27e55f566a7d05
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-10-11 00:16:36 +0530

    Migrates attacks to seperates file for better scalability

---
+++ scripts/data/Attacks.js
@@ -0,0 +1,15 @@
+const Attacks = {
+  Tackle: {
+    name: "tackle",
+    type: "normal",
+    damage: 10,
+  },
+  
+  FireBall: {
+    name: "fireball",
+    type: "fire",
+    damage: 15,
+  },
+};
+
+export default Attacks;
